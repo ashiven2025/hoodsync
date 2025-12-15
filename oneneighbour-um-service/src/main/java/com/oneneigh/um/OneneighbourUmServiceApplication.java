d1bd95bf83c93e1d39e67f2fa7d407781396b664
Kotesh chappidi <koteswara.chappidi@gmail.com>
2025-12-15 00:25:59 -0500

    initial commit

---
+++ oneneighbour-um-service/src/main/java/com/oneneigh/um/OneneighbourUmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oneneigh.um;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneneighbourUmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneneighbourUmServiceApplication.class, args);
+	}
+
+}
