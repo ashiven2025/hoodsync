e6b630a364e2c3a5465cec49e1d7d522c3afd0af
Kotesh chappidi <koteswara.chappidi@gmail.com>
2025-12-15 02:01:14 -0500

    added database service code

---
+++ oneneighbour-database-service/src/main/java/com/oneneigh/database/OneneighbourDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oneneigh.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneneighbourDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneneighbourDatabaseServiceApplication.class, args);
+	}
+
+}
